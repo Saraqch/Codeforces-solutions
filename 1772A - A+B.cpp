da703fe6cf70bcca0a32ac7cb46cbc2a170ee404
Saraqch <quispechoquesara@gmail.com>
2025-01-09 20:05:05 -0400

    1772A *800

---
+++ 1772A - A+B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define fast_cin()  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+int main(){
+    fast_cin();
+    int t;
+    cin >> t;
+    int a, b;
+    while(t--){
+        cin >> a >> b;
+        cout << a+b << endl;
+    }
+
+}
