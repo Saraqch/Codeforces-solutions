370f798760236c77248fe276150d70b7ca6771b0
Saraqch <quispechoquesara@gmail.com>
2025-03-18 13:33:02 -0400

    630A 800*

---
+++ 630A - Again Twenty Five.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define fast_cin() ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+int main(){
+    fast_cin();
+
+    long long n;
+    cin >> n;
+
+    long long potencia = pow(5, n);
+
+    cout << 25 << endl;
+}
